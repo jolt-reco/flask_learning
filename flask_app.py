3ce67e5996736797b89840570efb258ebd0ffb10
Rikuto Fukuda <fukuriku1205@gmail.com>
2025-07-16 20:49:19 +0900

    add:編集機能追加

--- flask_app.py
+++ flask_app.py
@@ -35,6 +35,35 @@ def list_items():
         items = []
     return render_template("list.html", items=items)
 
+# 指定行のデータを編集
+@app.route("/edit/<int:id>")
+def edit(id):
+    with open("item.csv", "r", encoding="utf-8")as f:
+        reader = list(csv.reader(f)) # CSVのデータをリスト化
+        if id < 0 or id >= len(reader):
+            return "対象がありません", 404
+        data = reader[id]
+        item = data[0]
+        price = data[1]
+    return render_template("edit.html", id=id, item=item, price=price) 
+    
+@app.route("/update", methods=["POST"]) 
+def update():
+    item = request.form["item"]
+    price = request.form["price"]
+    id = int(request.form["id"])
+
+    with open("item.csv", "r", encoding="UTF-8")as f:
+        data = list(csv.reader(f))
+    
+    data[id] = [item, price]
+
+    with open("item.csv", "w", newline="", encoding="UTF-8")as f:
+        writer = csv.writer(f)
+        writer.writerow(data)
+    
+    return redirect(url_for("list_items"))
+
 # 実行条件
 if __name__ == "__main__":
     app.run(debug=True)
